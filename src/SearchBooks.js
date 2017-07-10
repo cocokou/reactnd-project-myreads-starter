1a462246873f3d70d7035ec60533e6308e637bc9
coco <isco@163.com>
2017-07-10 23:41:46 +0800

    222

---
+++ src/SearchBooks.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom'
+
+export default class SearchBooks extends Component {
+    state = {
+    query: ''
+  }
+
+  render() {
+
+    // let showingBooks
+    // if (query) {
+    //   const match = new RegExp(escapeRegExp(query), 'i')
+    //   showingBooks = books.filter((book) => match.test(book.name))
+    // } else {
+    //   showingbooks = books
+    // }
+
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link className='close-search' to='/'>Close</Link>
+          <div className="search-books-input-wrapper">
+            <input type="text" placeholder="Search by title or author"/>
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
